c62ade4ef2d712a4ab3d4a2ffc2d44421dfd7d9c
YevheniiBond <interdancecoach@gmail.com>
2022-06-11 12:46:09 +0300

    first commit

---
+++ src/test/java/Test/Myaso.java
@@ -0,0 +1,15 @@
+package Test;
+
+import PageObj.MegaSupermarketPage;
+import org.testng.annotations.Test;
+
+public class Myaso extends TestInit {
+@Test
+    public void myaso(){
+        MegaSupermarketPage megaSupermarketPage = new MegaSupermarketPage(driver);
+        driver.get("https://megamarket.zakaz.ua/uk/categories/meat-fish-poultry-megamarket/");
+        megaSupermarketPage.listOfMeat().get(3).click();
+
+
+    }
+}
---
+++ src/test/java/Test/TestVarus.java
@@ -0,0 +1,31 @@
+package Test;
+
+import PageObj.NovusHomePage;
+import PageObj.VarusHomePage;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestVarus extends TestInit{
+    @Test
+    public void orderingFoodFromVarus(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+        VarusHomePage varusHomePage = new VarusHomePage(driver);
+        NovusHomePage novusHomePage = new NovusHomePage(driver);
+        Actions action = new Actions(driver);
+        goToZakaz();
+        maxWindow();
+        varusHomePage.goToVarus().click();
+        action.moveToElement(varusHomePage.alcohol()).perform();
+        varusHomePage.jimBeam().click();
+        varusHomePage.yesButton().click();
+        varusHomePage.whiteJimBeam1L().click();
+        varusHomePage.addToCartBtn().click();
+        novusHomePage.selfDelivery().click();
+        novusHomePage.addressSearchField().click();
+        novusHomePage.chooseCityNumber2().click();
+        novusHomePage.continueShopping().click();
+
+    }
+}
